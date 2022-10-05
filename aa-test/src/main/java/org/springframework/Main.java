d869db0317ed1b6f50f4d32c272b2c37fd5e0185
lla <944162982@qq.com>
2022-10-05 17:58:03 +0800

    依赖

--- aa-test/src/main/java/org/springframework/Main.java
+++ aa-test/src/main/java/org/springframework/Main.java
@@ -2,21 +2,18 @@ package org.springframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.concurrent.TimeUnit;
-
 public class Main {
-	public static void main(String[] args) throws Exception {
-	    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
-	    ac.register(Main.class);
-	    ac.refresh();
-	    try {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+		ac.register(Main.class);
+		ac.refresh();
+		try {
 			// 输出：org.springframework.context.annotation.internalConfigurationAnnotationProcessor
-	        System.out.println(ac.getBeanDefinitionNames()[0]);
-	    }catch (Exception e) {
-	        e.printStackTrace();
-	    } finally {
-	        TimeUnit.SECONDS.sleep(10);
-	        ac.close();
-	    }
+			System.out.println(ac.getBeanDefinitionNames()[0]);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ac.close();
+		}
 	}
 }
